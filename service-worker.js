99cb0ff943ba7f320e7fab71070f6788ee2d5494
Artem <101344894+tyomkaa@users.noreply.github.com>
2026-09-01 22:32:11 +0200

    v0.2.2: refresh PWA shell cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='job-radar-public-v2';
+const CACHE='job-radar-public-v3';
 const SHELL=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./icons/icon-192.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
